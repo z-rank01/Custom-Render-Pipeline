3627ca1b34e8d1a3b2d4c309d064135cc68fae34
z-rank01 <chenhd0110@qq.com>
2024-08-31 21:35:56 +0800

    2024/8/31 ChenHaoDong 1. Modify Unlit.hlsl to remove error syntax

--- Assets/HLSL/Unlit.hlsl
+++ Assets/HLSL/Unlit.hlsl
@@ -1,16 +1,5 @@
-// #include "UnityCG.cginc"
-
-#define UNITY_MATRIX_M unity_ObjectToWorld
-#define UNITY_MATRIX_I_M unity_WorldToObject
-#define UNITY_MATRIX_V unity_MatrixV
-#define UNITY_MATRIX_I_V unity_MatrixInvV
-#define UNITY_MATRIX_VP unity_MatrixVP
-#define UNITY_PREV_MATRIX_M unity_prev_MatrixM
-#define UNITY_PREV_MATRIX_I_M unity_prev_MatrixIM
-#define UNITY_MATRIX_P glstate_matrix_projection
-
-#include <Library/PackageCache/com.unity.render-pipelines.core@14.0.8/ShaderLibrary/SpaceTransforms.hlsl>
-#include <Library/PackageCache/com.unity.render-pipelines.core@14.0.8/ShaderLibrary/Common.hlsl>
+#ifndef CUSTOM_UNLIT_INCLUDED
+#define CUSTOM_UNLIT_INCLUDED
 
 struct appdata
 {
@@ -22,7 +11,7 @@ struct v2f
     float4 vertexCS : SV_POSITION;
 };
 
-// To reduce "setpass call", 
+// To reduce "setPass call", 
 // use constant buffer per material or draw macros
 // to cache the properties in GPU for reuse.
 
@@ -35,11 +24,10 @@ float4 _MainTex_ST;
 float4 _BaseColor;
 CBUFFER_END
 
-
 v2f vert(appdata v)
 {
     v2f o;
-    o.vertexCS = mul(unity_MatrixVP, mul(unity_ObjectToWorld, v.vertexOS));
+    o.vertexCS = UnityObjectToClipPos(v.vertexOS);
     return o;
 }
 
@@ -47,3 +35,5 @@ fixed4 frag(v2f i) : SV_Target
 {
     return _BaseColor;
 }
+
+#endif
